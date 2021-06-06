fc235f60fb498a4d2ab2fa186f318a1d2074f9ab
Dan Bumbarger <danb@alleninstitute.org>
2021-06-05 17:49:32 -0700

    Removed Qt dependencies

--- MakeWatertight/cleanup.cpp
+++ MakeWatertight/cleanup.cpp
@@ -3,15 +3,26 @@
 #include "complex.h"
 
 //#include <apps/QT/trimesh_QT_shared/mesh.h>
+#include<vcg/complex/complex.h>
+#include<vcg/complex/algorithms/stat.h>
 #include <vcg/complex/algorithms/clean.h>
 #include <vcg/complex/algorithms/hole.h>
+#include <vcg/complex/algorithms/mesh_to_matrix.h>//Triggers error with MinGW, but works in msvc??
+#include<vcg/complex/algorithms/inertia.h>
+
+//#include <eigenlib/Eigen/Core>
+
+
+
+//#include <igl/ambient_occlusion.h>
+//#include <igl/AABB.h>
 
 #include "mesh.h"
 
 using namespace vcg;
 using namespace std;
 
-void Cleanup::initialCleanup(CMeshO& mesh){
+void Cleanup::initialCleanup(CMeshO& mesh) {
     int result = 0;
     //No pre condition. Post condition MM_GEOMETRY_AND_TOPOLOGY_CHANGE (Not sure I have to do anything when this is used.)
     result = tri::Clean<CMeshO>::RemoveFaceOutOfRangeArea(mesh, 0);//This I believe causes some unfortunate holes...and susequent t-vertices, that I need to fix.
@@ -38,16 +49,51 @@ void Cleanup::initialCleanup(CMeshO& mesh){
     if (result != 0) updateBoxAndNormals(mesh);
 }
 
-void Cleanup::ambientOcclusionRemoval(CMeshO& mesh){
-//    int result;
-//    tri::Allocator<CMeshO>::CompactEveryVector(mesh);
-//    tri::UpdateSelection<CMeshO>::VertexClear(mesh);//I don't think I need to do faces?
-    //This is going to be a little complicated to implement, as I'll have to use meshlab plugin code rather than vcglib.
-    //https://github.com/cnr-isti-vclab/meshlab/blob/f33ff16ca1d78d1bcf46f838dba0b264aa45887d/src/meshlabplugins/filter_ao/filter_ao.cpp
-    cout << "ambinetOcclusionRemoval not yet implemented.";
+void Cleanup::ambientOcclusionRemoval(CMeshO& mesh) {
+    //https://github.com/cnr-isti-vclab/meshlab/blob/585226d8a28244379c2807b6848634c4c545c4b7/src/meshlabplugins/filter_ao/filter_ao.cpp
+    //Blender looks too tough for me to figure out
+    //OpenGl? Vulkan?
+    //This might be our best bet: https://github.com/libigl/libigl/blob/main/tutorial/606_AmbientOcclusion/main.cpp
+    //meshlab is actually using it underneath for some things, so I should be able to figure out type conversion.
+    //https://github.com/cnr-isti-vclab/vcglib/blob/001a01b38688acbf81f0ec821e9bde3e6d4622ab/wrap/igl/smooth_field.h
+    //They use a MeshToMatrix function to get the appropriate vertex and face matrices.
+
+
+
+
+    //Step 1: Get the matrices from the meshlab mesh.
+    int verts_deleted;
+    //    Eigen::MatrixXf V;
+    //    Eigen::MatrixXi F;
+    //    Eigen::MatrixXf NV; //Vertex Normals
+    //    Eigen::MatrixXf NF; //Face normals
+    //    Eigen::VectorXf AO; //Ambient Occlusion Values
+
+    //    vcg::tri::MeshToMatrix<CMeshO>::GetTriMeshData(mesh,F,V);
+    //    vcg::tri::MeshToMatrix<CMeshO>::GetNormalData(mesh, NV, NF);
+
+
+    //    Step two: Use libigl to calculate the ambient occlusion.
+        //I get some errors when I include <igl/ambient_occlusion.h>
+    //    igl::embree::ambient_occlusion(V,F,V,NV,2500,AO);
+    //    igl::ambient_occlusion(V,F,V,NV,2500,AO);
+
+
+        //Step three: I need to set the quality of the meshlab mesh to teh normalized ambient occlusion value.
+
+
+
+        //Step four: Delete faces and vertices based on that quality.
+
+
+    //    tri::Allocator<CMeshO>::CompactEveryVector(mesh);
+    //    tri::UpdateSelection<CMeshO>::VertexClear(mesh);//I don't think I need to do faces?
+        //This is going to be a little complicated to implement, as I'll have to use meshlab plugin code rather than vcglib.
+        //https://github.com/cnr-isti-vclab/meshlab/blob/f33ff16ca1d78d1bcf46f838dba0b264aa45887d/src/meshlabplugins/filter_ao/filter_ao.cpp
+    cout << "ambientOcclusionRemoval not yet implemented.";
 }
 
-void Cleanup::fixNonManifold(CMeshO& mesh){
+void Cleanup::fixNonManifold(CMeshO& mesh) {
     int iteration = 0;
 
     vcg::tri::UpdateTopology<CMeshO>::FaceFace(mesh);
@@ -57,13 +103,13 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
     tri::Allocator<CMeshO>::CompactVertexVector(mesh);
     tri::Allocator<CMeshO>::CompactFaceVector(mesh);
 
-    if((tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh) == 0) && (tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh) == 0))
+    if ((tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh) == 0) && (tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh) == 0))
     {
-        cout << "Mesh is 2-manifold at start of fixNonManifold. /n Closing holes." <<endl;
+        cout << "Mesh is 2-manifold at start of fixNonManifold. /n Closing holes." << endl;
         closeHoles(mesh);
         vcg::tri::UpdateTopology<CMeshO>::FaceFace(mesh);
         vcg::tri::UpdateTopology<CMeshO>::VertexFace(mesh);
-        if((tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh) == 0) && (tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh) == 0))
+        if ((tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh) == 0) && (tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh) == 0))
         {
             cout << "Mesh remains 2-manifold. Skipping fixNonManifold iteration." << endl;
             return;
@@ -74,7 +120,7 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
         }
     }
 
-    while(true)
+    while (true)
     {
         iteration++;
         int result = 0;
@@ -85,14 +131,13 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
         tri::Allocator<CMeshO>::CompactVertexVector(mesh);
         tri::Allocator<CMeshO>::CompactFaceVector(mesh);
         updateBoxAndNormals(mesh);
-        cout << endl <<"Beginning iteration " << iteration << " of fixNonManifold" << endl;
-        printf("Mesh has %i vertices and %i triangular faces\n",mesh.VN(),mesh.FN());
-
+        cout << endl << "Beginning iteration " << iteration << " of fixNonManifold" << endl;
+        printf("Mesh has %i vertices and %i triangular faces\n", mesh.VN(), mesh.FN());
 
         //No pre condition. Post condition MM_GEOMETRY_AND_TOPOLOGY_CHANGE (Not sure I have to do anything when this is used.)
         result = tri::Clean<CMeshO>::RemoveUnreferencedVertex(mesh);
         cout << result << " unreferenced vertices removed." << endl;
-        printf("Mesh has %i vertices and %i triangular faces\n",mesh.VN(),mesh.FN());
+        printf("Mesh has %i vertices and %i triangular faces\n", mesh.VN(), mesh.FN());
         if (result != 0) updateBoxAndNormals(mesh);
 
         //requirements MeshModel::MM_FACEFACETOPO | MeshModel::MM_VERTMARK;
@@ -106,19 +151,19 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
         tri::Allocator<CMeshO>::CompactVertexVector(mesh);
         tri::Allocator<CMeshO>::CompactFaceVector(mesh);
 
-        cout  << "Repairing non-manifold edges through vertex splitting." << endl;
-        printf("Mesh has %i vertices and %i triangular faces after update.\n",mesh.VN(),mesh.FN());
+        cout << "Repairing non-manifold edges through vertex splitting." << endl;
+        printf("Mesh has %i vertices and %i triangular faces after update.\n", mesh.VN(), mesh.FN());
         //requirements MeshModel::MM_FACEFACETOPO | MeshModel::MM_VERTMARK;
         //Post condition MM_GEOMETRY_AND_TOPOLOGY_CHANGE
         result = tri::Clean<CMeshO>::SplitManifoldComponents(mesh/*, .005*/);//I need to add a move threshold
         cout << result << " vertices split by SplitManifoldComponents." << endl;
         //This results in a crazy number of unreferenced vertices! Is this a bug?
-        printf("Mesh has %i vertices and %i triangular faces post vertex splitting. \n",mesh.VN(),mesh.FN());
+        printf("Mesh has %i vertices and %i triangular faces post vertex splitting. \n", mesh.VN(), mesh.FN());
 
         //No pre condition. Post condition MM_GEOMETRY_AND_TOPOLOGY_CHANGE (Not sure I have to do anything when this is used.)
         result = tri::Clean<CMeshO>::RemoveUnreferencedVertex(mesh);
         cout << result << " unreferenced vertices removed." << endl;
-        printf("Mesh has %i vertices and %i triangular faces\n",mesh.VN(),mesh.FN());
+        printf("Mesh has %i vertices and %i triangular faces\n", mesh.VN(), mesh.FN());
         if (result != 0) updateBoxAndNormals(mesh);
 
         vcg::tri::UpdateTopology<CMeshO>::FaceFace(mesh);
@@ -130,13 +175,13 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
         cout << "Repairing non-manifold vertices by vertex splitting." << endl;
         result = tri::Clean<CMeshO>::SplitNonManifoldVertex(mesh, .005);
         cout << result << " vertices split by SplitManifoldVertex." << endl;
-        printf("Mesh has %i vertices and %i triangular faces post vertex splitting. \n",mesh.VN(),mesh.FN());
+        printf("Mesh has %i vertices and %i triangular faces post vertex splitting. \n", mesh.VN(), mesh.FN());
         if (result != 0) updateBoxAndNormals(mesh);
 
         //No pre condition. Post condition MM_GEOMETRY_AND_TOPOLOGY_CHANGE (Not sure I have to do anything when this is used.)
         result = tri::Clean<CMeshO>::RemoveUnreferencedVertex(mesh);
         cout << result << " unreferenced vertices removed." << endl;
-        printf("Mesh has %i vertices and %i triangular faces\n",mesh.VN(),mesh.FN());
+        printf("Mesh has %i vertices and %i triangular faces\n", mesh.VN(), mesh.FN());
         if (result != 0) updateBoxAndNormals(mesh);
 
         //select and delete remaining
@@ -157,13 +202,13 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
 
         Cleanup::updateBoxAndNormals(mesh);
 
-//        tri::UpdateSelection<CMeshO>::VertexClear(mesh);
-//        tri::UpdateSelection<CMeshO>::FaceClear(mesh);
+        //        tri::UpdateSelection<CMeshO>::VertexClear(mesh);
+        //        tri::UpdateSelection<CMeshO>::FaceClear(mesh);
 
         int nonmanivert = tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh, false, true);
         int nonmaniedge = tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh);
         cout << nonmaniedge << " non-manifold edges and " << nonmanivert << " non-manifold vertices." << endl;
-        if((nonmanivert == 0) && (nonmaniedge == 0))
+        if ((nonmanivert == 0) && (nonmaniedge == 0))
         {
             cout << "Mesh is now 2-manifold" << endl;;
             //Close holes and update
@@ -185,7 +230,7 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
             //Check for 2-manifold again.
             tri::UpdateSelection<CMeshO>::VertexClear(mesh);
             tri::UpdateSelection<CMeshO>::FaceClear(mesh);
-            if((tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh, false, true)) == 0 && (tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh) == 0))
+            if ((tri::Clean<CMeshO>::CountNonManifoldVertexFF(mesh, false, true)) == 0 && (tri::Clean<CMeshO>::CountNonManifoldEdgeFF(mesh) == 0))
             {
                 cout << "Finished fixNonManifold." << endl;
                 break;
@@ -199,18 +244,18 @@ void Cleanup::fixNonManifold(CMeshO& mesh){
 }
 
 //This immitates Meshlab. Defined in meshmodel.cpp
-void Cleanup::updateBoxAndNormals(CMeshO& mesh){
+void Cleanup::updateBoxAndNormals(CMeshO& mesh) {
     tri::UpdateBounding<CMeshO>::Box(mesh);
 
-//meshlab uses mesh.fn instead of mesh.FN(), though it tells you never to use fn
-    if(mesh.FN()>0) {
+    //meshlab uses mesh.fn instead of mesh.FN(), though it tells you never to use fn
+    if (mesh.FN() > 0) {
         mesh.vert.EnableNormal();
         tri::UpdateNormal<CMeshO>::PerFaceNormalized(mesh);
         tri::UpdateNormal<CMeshO>::PerVertexAngleWeighted(mesh);
     }
 }
 
-int Cleanup::deleteSelectedVerts(CMeshO& mesh){
+int Cleanup::deleteSelectedVerts(CMeshO& mesh) {
     //Flag as deleted all vertices that are flagged as selected.
     //Might be more effecient for me to re-write CountNonManifoldVertexFF and flag as delete instead of selected.
     CMeshO::FaceIterator faceIterator;
@@ -231,18 +276,18 @@ int Cleanup::deleteSelectedVerts(CMeshO& mesh){
             tri::Allocator<CMeshO>::DeleteVertex(mesh, *vertexIterator);
             deleted++;
         }
-//    m.clearDataMask(MeshModel::MM_FACEFACETOPO);
-//    m.clearDataMask(MeshModel::MM_VERTFACETOPO);
+    //    m.clearDataMask(MeshModel::MM_FACEFACETOPO);
+    //    m.clearDataMask(MeshModel::MM_VERTFACETOPO);
     updateBoxAndNormals(mesh);
     printf("Deleted %i vertices, %i faces.", vvn - mesh.vn, ffn - mesh.fn);
 
     return deleted;
 }
 
-void Cleanup::closeHoles(CMeshO& mesh, int maxHoleSize, bool selected , bool avoidSelfIntersection){
+void Cleanup::closeHoles(CMeshO& mesh, int maxHoleSize, bool selected, bool avoidSelfIntersection) {
     cout << "Closing holes." << endl;
-    if(avoidSelfIntersection)
-        tri::Hole<CMeshO>::EarCuttingIntersectionFill<tri::SelfIntersectionEar< CMeshO> >(mesh,maxHoleSize,selected);
+    if (avoidSelfIntersection)
+        tri::Hole<CMeshO>::EarCuttingIntersectionFill<tri::SelfIntersectionEar< CMeshO> >(mesh, maxHoleSize, selected);
     else
-        tri::Hole<CMeshO>::EarCuttingFill<vcg::tri::MinimumWeightEar< CMeshO> >(mesh,maxHoleSize,selected);
+        tri::Hole<CMeshO>::EarCuttingFill<vcg::tri::MinimumWeightEar< CMeshO> >(mesh, maxHoleSize, selected);
 }
---
+++ MakeWatertight/makewatertight.cpp
@@ -0,0 +1,102 @@
+//vcglib uses some pre-c++11 code practices that are now considered either unsafe or deprecated. 
+//To compile correctly with MSVC, the /permissive option must be used.
+//In visual studio, find it under c/c++::language::Conformance mode
+//_CRT_SECURE_NO_WARNINGS must also be defined as a preprocessor command. 
+
+#include "boost/program_options.hpp"
+#include "boost/filesystem.hpp"
+#include <wrap/io_trimesh/import.h>
+#include <wrap/io_trimesh/export.h>
+
+#include "mesh.h"
+#include "cleanup.h"
+
+#include <iostream>
+using namespace std;
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+    po::options_description description("Takes a trimesh as input and outputs a watertight and 2-manifold mesh.");
+
+    description.add_options()
+        ("help, h", "Display this help message.")
+
+        ("source, s",
+            po::value<string>()->default_value("tiny.obj"),
+            "Source file.")
+
+        ("destination, d",
+            po::value<string>()->default_value("output"),
+            "Destination folder. Will create if does not exist.")
+
+        ("ambient_occlusion, a",
+            po::value<bool>()->default_value(true),
+            "Deletion of internal geometries based on screen space ambient occlusion values. Default True.")
+        ;
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(description).run(), vm);
+    po::notify(vm);
+
+    boost::filesystem::path source{vm["source"].as<string>()};
+    boost::filesystem::path destination{vm["destination"].as<string>()};
+
+    if(!boost::filesystem::exists(destination))
+    {
+        cout << "Creating destination directory:" << destination.string() <<  endl;
+        boost::filesystem::create_directory(vm["destination"].as<string>());
+    }
+
+    CMeshO mesh;
+    mesh.face.EnableNormal();
+    mesh.vert.EnableNormal();
+
+        //Load file
+
+    if(!boost::filesystem::exists(source.string())) {
+        cout << "Source file does:" << source.root_path() << " does not exist!" << endl;
+        cout << "current path: " << boost::filesystem::current_path() << endl;
+        return 1;
+    }
+
+    int loadmask = 0;
+    if (!vcg::tri::io::Importer<CMeshO>::LoadMask(source.string().data(), loadmask)){
+        cout << "Error while loading mask." << endl;
+    }
+    vcg::tri::io::Importer<CMeshO>::Open(mesh, source.string().data(), loadmask);
+    printf("Mesh has %i vertices and %i triangular faces\n",mesh.VN(),mesh.FN());
+
+        //Enable CMeshO modules and update.
+        mesh.face.EnableFFAdjacency();
+        mesh.vert.EnableVFAdjacency();
+        mesh.face.EnableVFAdjacency();
+
+        vcg::tri::UpdateTopology<CMeshO>::FaceFace(mesh);
+        vcg::tri::UpdateTopology<CMeshO>::VertexFace(mesh);
+        vcg::tri::UpdateFlags<CMeshO>::FaceBorderFromFF(mesh);
+
+        mesh.vert.EnableMark();
+        mesh.vert.EnableQuality();
+        mesh.vert.EnableCurvature();
+        mesh.vert.EnableCurvatureDir();
+        mesh.vert.EnableRadius();
+
+        //Process Mesh
+        Cleanup::initialCleanup(mesh);
+        //if(parser.isSet(ambientOcclusionOption))
+        //{
+        //    Cleanup::ambientOcclusionRemoval(mesh);
+        //}
+        Cleanup::fixNonManifold(mesh);
+        Cleanup::closeHoles(mesh);
+
+        //Save Mesh
+        std::string outputFilePath = (destination.string() + "/" + source.filename().string());
+        cout << "saving output to " << outputFilePath << endl;
+        vcg::tri::io::Exporter<CMeshO>::Save(mesh, outputFilePath.c_str());
+
+        cout << "Finished!";
+        return 0;
+}
+
